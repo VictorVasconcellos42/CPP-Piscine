9263ec8eee7f03d7ddc3646ad10bc5e965059b53
Victor Vasconcellos <victorvasconcellos42@gmail.com>
2023-07-12 17:23:45 -0300

    Fix: new megaphone function created

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,28 +6,25 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 13:18:32 by vde-vasc          #+#    #+#             */
-/*   Updated: 2023/04/22 13:40:08 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2023/07/12 17:09:09 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
-#include <algorithm>
 
 int main(int argc, char **argv)
 {
-    if (!argv[1])
-    {
-        std::cout << "INSERT TEXT" << std::endl;
-        return (0);
-    }
-    for (int i = 1; i < argc; i++)
-    {
-        std::string str(argv[i]);
-        std::transform(str.begin(), str.end(), str.begin(), ::toupper);
-        if (i == argc -1)
-            std::cout << str;
-        else
-            std::cout << str << " ";
-    }
+	if (!argv[1])
+	{
+		std::cout << "INSERT TEXT" << std::endl;
+		return (0);
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		std::string str(argv[i]);
+		for (int j = 0; str[j]; j++)
+			str[j] = toupper(str[j]);
+		std::cout << str;
+	}
 }
