010d240209606fb59cf441b0e9287952e8409ef5
Victor Vasconcellos <victorvasconcellos42@gmail.com>
2023-07-17 21:23:08 -0300

    Feat: Contact class created

--- CPP00/ex01/Contact.hpp
+++ CPP00/ex01/Contact.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   contact.hpp                                        :+:      :+:    :+:   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/22 14:06:03 by vde-vasc          #+#    #+#             */
-/*   Updated: 2023/04/22 17:30:16 by vde-vasc         ###   ########.fr       */
+/*   Created: 2023/07/16 18:28:10 by vde-vasc          #+#    #+#             */
+/*   Updated: 2023/07/17 01:44:32 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,26 @@
 
 class Contact
 {
-    public:
-        Contact();
-        ~Contact();
+	private:
+		std::string firstName;
+		std::string lastName;
+		std::string nickname;
+		std::string phoneNumber;
+		std::string darkestSecret;
 
-        std::string getNickName() const;
-        std::string getFirstName() const;
-        std::string getLastName() const;
-        std::string getPhoneNumber() const;
-        std::string getDarkestSecret() const;
-        
-        void setNickName(const std::string);
-        void setFirstName(const std::string);
-        void setLastName(const std::string);
-        void setDarkestSecret(const std::string);
-        void setPhoneNumber(const std::string);
-
-    private:
-        std::string firstName;
-        std::string lastName;
-        std::string nickName;
-        std::string darkestSecret;
-        std::string phoneNumber;
-};
+	public:
+		Contact();
+		~Contact();
+		std::string getFirstName(void) const;
+		std::string getLastName(void) const;
+		std::string getNickname(void) const;
+		std::string getPhoneNumber(void) const;
+		std::string getDarkestSecret(void) const;
+		void setFirstName(std::string firstName);
+		void setLastName(std::string lastName);
+		void setNickname(std::string nickname);
+		void setPhoneNumber(std::string phoneNumber);
+		void setDarkestSecret(std::string darkestSecret);
+} ;
 
 #endif
