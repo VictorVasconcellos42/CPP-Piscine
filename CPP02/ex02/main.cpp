8a591e133e564f349ff45bd11dac70a1f8b93efc
Victor Vasconcellos <victorvasconcellos42@gmail.com>
2023-08-01 17:19:10 -0300

    Feat: new main of tests created

--- CPP02/ex02/main.cpp
+++ CPP02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 02:13:38 by vde-vasc          #+#    #+#             */
-/*   Updated: 2023/07/26 14:27:10 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2023/08/01 17:10:40 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,23 @@
 
 int main( void ) 
 {
-	Fixed a;
+	Fixed a(20);
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << "------------------------------------" << std::endl;
+	std::cout << "Arithmetic Operation: " << std::endl;
+	std::cout << "Operator << result: " << a << std::endl;
+	std::cout << "Operator + result: " << a + b << std::endl;
+	std::cout << "Operator - result: " << a - b << std::endl;
+	std::cout << "Operator * result: " << a * b << std::endl;
+	std::cout << "Operator / result: " << (a / b) << std::endl;
+	std::cout << "Operator  result: " << b << std::endl;
+	std::cout << "------------------------------------" << std::endl;
+	std::cout << "Max and Min Operation: " << std::endl;
+	std::cout << "A value: " << a << std::endl;
+	std::cout << "B value: " << b << std::endl;
+	std::cout << "Min between A and B result: " << Fixed::min(a, b) << std::endl;
+	std::cout << "Max between A and B result: " << Fixed::max( a, b ) << std::endl;
+	std::cout << "------------------------------------" << std::endl;
 	return 0;
 }
